589adceb8997fb2dfc76a37a00554af88302f6a5
Snow_Z <kzhang36@buffalo.edu>
2025-07-28 17:03:24 +0800

    Add files via upload

---
+++ backend/SeismicGisApplication.java
@@ -0,0 +1,9 @@
+package com.example.seismicgis;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SeismicGisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeismicGisApplication.class, args);
+    }
+}
